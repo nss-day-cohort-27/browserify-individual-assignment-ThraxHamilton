97813977fde65d64f90f6c0a7f27a51e1d2bc598
ThraxHamilton <mmcclenton16@gmail.com>
2018-08-21 12:07:11 -0500

    fixed background, added googlefonts

--- src/js/cityEntry.js
+++ src/js/cityEntry.js
@@ -2,7 +2,7 @@ function createEnrty(city, state, why, cityId) {
     return ` <div class="card" style="width: 18rem;">
     <div class="card-body">
       <h1 class="cityName">${city}</h1>
-      <p class="state">${state}</p>
+      <h3 class="state">${state}</h3>
       <p class="cityWhy">${why}</p>
       <button id="deleteButton--${cityId}">Delete City</button>
     </div>
--- src/js/poiEntry.js
+++ src/js/poiEntry.js
@@ -2,7 +2,7 @@ function createPoiEnrty(place, location, why, poiID){
     return `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h1 class="poi-place">${place}</h5>
-      <p class="poi-location">${location}</p>
+      <h3 class="poi-location">${location}</h3>
       <p class="poi-why">${why}</p>
       <button id="deletePoiButton--${poiID}">Delete Place</button>
     </div>
